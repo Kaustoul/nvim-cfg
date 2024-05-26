34a10146f4baa44172694ec1a9731c522952fc26
Radek Sverak <rasvek@gmail.com>
2024-05-26 11:04:38 +0200

    Added copilot plugin

--- lua/frajer/packer.lua
+++ lua/frajer/packer.lua
@@ -4,43 +4,44 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use { "catppuccin/nvim", as = "catppuccin" }
+    use { "catppuccin/nvim", as = "catppuccin" }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  use 'nvim-lua/plenary.nvim' 
-  use 'ThePrimeagen/harpoon'
-  use 'mbbill/undotree'
-  use 'tpope/vim-fugitive'
+    use 'nvim-lua/plenary.nvim' 
+    use 'ThePrimeagen/harpoon'
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
 
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path' },
-    {'hrsh7th/cmp-cmdline'},
-    {'saadparwaiz1/cmp_luasnip'},
-    -- Snippets
-    {
-	    'L3MON4D3/LuaSnip',
-	    dependencies = { "rafamadriz/friendly-snippets" },
-    },
-  }
-}
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path' },
+            {'hrsh7th/cmp-cmdline'},
+            {'saadparwaiz1/cmp_luasnip'},
+            -- Snippets
+            {
+                'L3MON4D3/LuaSnip',
+                dependencies = { "rafamadriz/friendly-snippets" },
+            },
+        },
+        use { 'github/copilot.vim' },
+    }
 end)
